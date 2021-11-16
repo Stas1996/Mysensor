5e2f5cdd246ae86c2047ee89e98b657f9ddd8c1f
stanislavk <stas.krot1996@gmail.com>
2021-11-16 09:50:51 +0200

    Light over 15000, charging by AC adapter, percentage equals to 55

--- app/src/main/java/com/example/mysemspr/MainActivity.java
+++ app/src/main/java/com/example/mysemspr/MainActivity.java
@@ -16,9 +16,8 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager sensorManager;
     private Sensor lightSensor;
-    private Sensor rotationSensor;
     private float light;
-    private float rotationZ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +25,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-
-
     }
 
     @Override
@@ -42,11 +38,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onResume() {
         super.onResume();
         sensorManager.registerListener(this,lightSensor,SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, rotationSensor,SensorManager.SENSOR_DELAY_NORMAL);
-
-
-
-
     }
 
     private boolean howBatteryCharging(Intent batteryStatus) {
@@ -54,35 +45,36 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         return chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
     }
 
-    private boolean isBatteryCharging(Intent batteryStatus) {
-        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                status == BatteryManager.BATTERY_STATUS_FULL;
-        return isCharging;
-    }
-
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if(event.sensor == lightSensor){
+        if(event.sensor == lightSensor) {
             light = event.values[0];
-        }else if(event.sensor == rotationSensor) {
-            rotationZ = event.values[0];
         }
         IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
         Intent batteryStatus = this.registerReceiver(null, ifilter);
-
-        boolean isCharging = isBatteryCharging(batteryStatus);
-
-// How are we charging?
         boolean acCharging = howBatteryCharging(batteryStatus);
-        Log.d("stas", rotationZ + "");
-        Log.d("stas",isCharging + " is charging");
+
         Log.d("stas",acCharging +" how charging");
         Log.d("stas", light + " light");
-        if(light > 6000 && isCharging && !acCharging && rotationZ > 0.680 && rotationZ < 0.690){
+
+        int percentage = batteryPrecentage(batteryStatus);
+        Log.d("stas", "percentage " + percentage);
+
+        if(light > 15000 && acCharging && percentage == 55){
             Intent intent = new Intent(MainActivity.this,SuccessActivity.class);
             startActivity(intent);
+            finish();
         }
+    }
+
+    private int batteryPrecentage(Intent batteryStatus) {
+
+        int level = batteryStatus != null ? batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) : -1;
+        int scale = batteryStatus != null ? batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1) : -1;
+
+        double batteryPct = level / (double) scale;
+
+        return (int) (batteryPct * 100);
     }
 
     @Override
